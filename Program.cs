e40d7b2251df64c63a83eec359b938e9e43e69e2
AlexArchanfel <lentao_07@mail.ru>
2022-10-18 03:16:22 +0300

    Home27

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27 Напишите функцию и запустите ее, которая принимает на вход число и выдает сумму цифр в числе
+
+int Roma(int R)
+{
+    int res = 0;
+
+    while (R > 0)
+    {
+        res = res + (R % 10);
+        R = R / 10;
+    }
+    return res;
+}
+Console.Clear();
+Console.Write ("Введите число: ");
+int R = int.Parse (Console.ReadLine ()!);
+Console.WriteLine($"Сумма цифр числа {R} равна {Roma(R)}");
